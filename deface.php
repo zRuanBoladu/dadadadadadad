b4328e9ee25bee7c020ad830abe784896059da69
zRuanBoladu <thelionbeast1234.phb@gmail.com>
2016-06-04 17:54:12 +0000

    crcr
    crcrc

---
+++ deface.php
@@ -0,0 +1,28 @@
+
+<META NAME="ROBOTS" CONTENT="INDEX, NOFOLLOW">
+<head>
+<title>HacKeD by RuanHacking</title>
+<link rel="shortcut icon" href="https://cdn2.iconfinder.com/data/icons/social-flat-buttons-3/512/anonymous-128.png">
+<script type="text/javascript">
+//<![CDATA[
+try{if (!window.CloudFlare) {var CloudFlare=[{verbose:0,p:0,byc:0,owlid:"cf",bag2:1,mirage2:0,oracle:0,paths:{cloudflare:"/cdn-cgi/nexp/dok3v=1613a3a185/"},atok:"fa78288ba0f300942d0f16c80fe2c8da",petok:"b840542b2c6b34482eb25901878a3e8c24135ef0-1465043098-1800",zone:"pagebin.com",rocket:"a",apps:{},sha2test:0}];document.write('<script type="text/javascript" src="//ajax.cloudflare.com/cdn-cgi/nexp/dok3v=e982913d31/cloudflare.min.js"><'+'\/script>');}}catch(e){};
+//]]>
+</script>
+
+<style>body{background:url(http://1.bp.blogspot.com/-uMDTFqVxfpU/VoUNVl6xVrI/AAAAAAAAFVk/FbvB_3E6hQw/s1600/anonymous-gif.gif) no-repeat center center fixed;-webkit-background-size:cover;-moz-background-size:cover;-o-background-size:cover;background-size:cover;color:white;}.line1{font-family:Arial,sans-serif;font-size:50px;text-shadow:grey 0px 0px 4px;}.line2{font-family:Arial;font-size:30px;text-shadow:grey 0px 0px 4px;}.line3{font-family:HELVETICA;font-size:43px;text-shadow:grey 0px 0px 4px;}.line4{font-family:HELVETICA;font-size:19px;text-shadow:grey 0px 0px 4px;}.line5{font-family:HELVETICA;font-size:23px;text-shadow:grey 0px 0px 4px;}.line6{font-family:Arial,sans-serif;font-size:34px;text-shadow:darkred 0px 0px 4px;}.twitter a{text-decoration:none;font-family:Arial,sans-serif;font-size:32px;}
+}</style>
+</head>
+<body><center>
+<table width=100% height=100%><td align=center>
+<DIV class=line1><font color=Greem><BIG><strong> HackeD By RuanHacking </strong></BIG></div>
+<DIV class=line2><font color=white><BIG> Site Totalmente Desprotegido Tudo e Possivel!</BIG></div>
+<DIV class=line3><font color=Greem><BIG> ~ /You've been Hacked ~</BIG></div>
+<DIV class=line1><font color=Red><BIG><strong> NINGUEM </strong></BIG></div>
+<DIV class=line2><font color=Blue><BIG> VAI TE DAR</BIG></div>
+<DIV class=line4><font color=Blue><BIG>A LIBERDADE</BIG></div>
+<DIV class=line1><font color=Red><BIG><strong> SEU LIXO! </strong></BIG></div>
+ <marquee align="center" scrollamount="5" width="560"><font color="red" size="-1">Salve - @Lawliet - Pain - Killer TUFF - <a class="__cf_email__" href="/cdn-cgi/l/email-protection" data-cfemail="cb87aaa6a6aeb98bb9a4a4bf">[email&#160;protected]</a><script data-cfhash='f9e31' type="text/javascript">/* <![CDATA[ */!function(t,e,r,n,c,a,p){try{t=document.currentScript||function(){for(t=document.getElementsByTagName('script'),e=t.length;e--;)if(t[e].getAttribute('data-cfhash'))return t[e]}();if(t&&(c=t.previousSibling)){p=t.parentNode;if(a=c.getAttribute('data-cfemail')){for(e='',r='0x'+a.substr(0,2)|0,n=2;a.length-n;n+=2)e+='%'+('0'+('0x'+a.substr(n,2)^r).toString(16)).slice(-2);p.replaceChild(document.createTextNode(decodeURIComponent(e)),c)}p.removeChild(t)}}catch(u){}}()/* ]]> */</script> - NikkiHacker - E A Rapa não citada,É NOIS!!</marquee>
+<iframe width="0" height="0" src="https://albireo1.sscdn.co/palcomp3/c/c/9/8/tribodaperiferia-inocente-118a67ea.mp3" frameborder="0" allowfullscreen></iframe>
+ <div id=bar style="position: fixed; width: 100%; bottom: 0px; font-family: tahoma; height: 20px; color: green; font-size: 13px; left: 0px; border-top: 1px solid #222; padding: 5px; background-color: #222"> 
+<CENTEr><B>>Copyright © 2016 RuanHacking# All Rights Reserved<</B></CENTER></div>
+</body>
